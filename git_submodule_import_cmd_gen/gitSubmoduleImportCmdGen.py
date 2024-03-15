f2ffee6d01f4b8213d8edd6e833c4e3cac7f2f02
prgrmz07 <prgrmz07@163.com>
2024-03-16 01:35:33 +0800

    Revert "sh直接执行git子模块命令，然而并没有获得多余的子模块，所有45和30到底咋回事？难道是45是有重复的？"
    This reverts commit 2377baf77db2f726f38e4fb33d04f64e3a98d8f0.

--- git_submodule_import_cmd_gen/gitSubmoduleImportCmdGen.py
+++ git_submodule_import_cmd_gen/gitSubmoduleImportCmdGen.py
@@ -4,6 +4,8 @@
 # 【文件作用】 遍历给定git仓库中的子模块列表， 并打印对应命令import_githubRepo_to_gitee.sh
 
 from pathlib import Path
+import git
+from git import cmd
 import shutil
 import re
 import random
@@ -11,12 +13,8 @@ import random
 import sys
 import argparse
 
-MINI_sleep_seconds = 1
-MINI_sleep_seconds_delta = 2
-
-
-# MINI_sleep_seconds = 8
-# MINI_sleep_seconds_delta = 5
+MINI_sleep_seconds = 8
+MINI_sleep_seconds_delta = 5
 
 parser = argparse.ArgumentParser(
 prog=f'gitSubmoduleImportCmdGen.py',
@@ -30,7 +28,7 @@ args=parser.parse_args()
 
 
 # print(sys.argv)
-repoPath:str=args.parent_repo_dir
+_BgRp:str=args.parent_repo_dir
 giteeMirrorOrgName=args.goal_org #"imagg"
 _sleepSeconds:str=args.sleep_seconds
 _sleepSecEnd:str=args.sleep_seconds+args.sleep_seconds_delta
@@ -38,34 +36,29 @@ _sleepSecEnd:str=args.sleep_seconds+args.sleep_seconds_delta
 errMsg=f"断言失败【 sleep_seconds>={MINI_sleep_seconds} 】且【【 sleep_seconds_delta>={MINI_sleep_seconds_delta} 】】"
 assert _sleepSeconds >= MINI_sleep_seconds and args.sleep_seconds_delta >= MINI_sleep_seconds_delta, errMsg
 
+BgRp:git.Repo=git.Repo(path=_BgRp)
+BgRpRmt:git.Remote=BgRp.remote()
+originUrlBgRp:str=BgRpRmt.url
+# BgRp_:cmd.Git=BgRp.git
 
-import sh
-# command = "find /fridaAnlzAp/pytorch/ -type f -name .gitmodules | xargs -I@ git --no-pager config --file @ --get-regexp url | awk '{ print $2 }' "
-command = "find __repoPath__ -type f -name .gitmodules | xargs -I@ git --no-pager config --file @ --get-regexp url | awk '{ print $2 }' "
-command=command.replace("__repoPath__",repoPath)
-output:str = sh.sh("-c", command)
-url_ls=output.splitlines(keepends=False)
-# print(len(url_ls))
-url_ls=list(filter(lambda k: k.startswith("http://") or k.startswith("https://"), url_ls))
-# print(len(url_ls))
-
-
+repoK:git.Submodule
 #                    //host/org /repo
 urlReExpr=r"http[s]?://(.+)/(.+)/(.+)\.git"
 _GIT=".git"
-for urlK in url_ls:
+for k,repoK in enumerate( BgRp.submodules):
     # print(f"{repoK.name}, {repoK.path}, {repoK.url}, {repoK.hexsha}, {repoK.branch_name}, {repoK.branch_path}")
+    urlK=repoK.url
     urlK=urlK if urlK.endswith(_GIT) else f"{urlK}{_GIT}"
     urlMatch=re.match(pattern=urlReExpr,string=urlK)
-    assert urlMatch is not None,f"断言失败，【{urlK}】不匹配正则表达式【{urlReExpr}】【忽略末尾.git】"
+    assert urlMatch is not None,f"断言失败，【{repoK.url}】不匹配正则表达式【{urlReExpr}】【忽略末尾.git】"
     urlFieldLs=urlMatch.groups()
-    assert urlFieldLs is not None and len(urlFieldLs) == 3 ,f"断言失败，【{urlK}】不匹配正则表达式【{urlReExpr}】【忽略末尾.git】，字段个数不为3，实际字段个数【{len(urlFieldLs)}】"
+    assert urlFieldLs is not None and len(urlFieldLs) == 3 ,f"断言失败，【{repoK.url}】不匹配正则表达式【{urlReExpr}】【忽略末尾.git】，字段个数不为3，实际字段个数【{len(urlFieldLs)}】"
     host=urlFieldLs[0]
     orgName=urlFieldLs[1]
     repoName=urlFieldLs[2]
     newRepoName=f"{orgName}--{repoName}"
     #import_githubRepo_to_gitee.sh --from_repo https://github.com/pytorch/pytorch.git  --goal_org imagg  --goal_repoPath pytorch--pytorch --goal_repoName pytorch--pytorch  --goal_repoDesc 来源https://github.com/pytorch/pytorch.git
-    cmd_import2gitee=f"import_githubRepo_to_gitee.sh --from_repo {urlK}  --goal_org {giteeMirrorOrgName}  --goal_repoPath {newRepoName} --goal_repoName {newRepoName}  --goal_repoDesc 【镜像】{urlK}; #sleep {random.randint(_sleepSeconds,_sleepSecEnd)}"
+    cmd_import2gitee=f"import_githubRepo_to_gitee.sh --from_repo {urlK}  --goal_org {giteeMirrorOrgName}  --goal_repoPath {newRepoName} --goal_repoName {newRepoName}  --goal_repoDesc 【镜像】{urlK}; sleep {random.randint(_sleepSeconds,_sleepSecEnd)}"
     print(cmd_import2gitee)
 
 _end=True
