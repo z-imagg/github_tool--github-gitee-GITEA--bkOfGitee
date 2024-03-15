1b20471f85ef77425abf118735b999f452e22f43
prgrmz07 <prgrmz07@163.com>
2024-03-15 18:39:14 +0800

    草稿

--- localGitea_as_github/repoMigrateToGitea.py
+++ localGitea_as_github/repoMigrateToGitea.py
@@ -8,12 +8,18 @@ import argparse
 from  RepoMigrateToGiteaFunc import giteaMigrateApi
 from gitea_api_cfg import gitea_host, gitea_port, gitea_token
 
-parser = argparse.ArgumentParser(
-prog=f'repoMigrateToGitea.py',
-description='【迁移给定git仓库url到gitea服务】')
 
-parser.add_argument('-f', '--from_repo_url',required=True,type=str,help="【父仓库url,常为gitee仓库】",metavar='')
-args=parser.parse_args()
+def giteaMigrateApiCmd():
+    parser = argparse.ArgumentParser(
+    prog=f'repoMigrateToGitea.py',
+    description='【迁移给定git仓库url到gitea服务】')
+
+    parser.add_argument('-f', '--from_repo_url',required=True,type=str,help="【父仓库url,常为gitee仓库】",metavar='')
+    args=parser.parse_args()
 
 
-giteaMigrateApi(args.from_repo_url, gitea_host,gitea_port, gitea_token)
+    giteaMigrateApi(args.from_repo_url, gitea_host,gitea_port, gitea_token)
+
+
+if __name__=="__main__":
+    giteaMigrateApiCmd()
