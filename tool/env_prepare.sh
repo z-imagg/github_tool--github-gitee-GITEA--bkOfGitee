c7c51c47ea052693312a82a799a38ac317d29e98
prgrmz07 <prgrmz07@163.com>
2024-03-15 13:17:27 +0800

    fix 子模块遍历

--- tool/env_prepare.sh
+++ tool/env_prepare.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 #获得 当前脚本所在的目录 的绝对路径，  不依赖于工作目录在哪里
-_script='source <(curl -s  giteaz:3000/bal/bash-simplify/raw/commit/2ee8e5286f925e44d0081ac10beee497e0245ded/dir_util.sh) ; getCurScriptDirByConcat _dir _fn && echo "D=${CurScriptDir}; F=${CurScriptNm}" '
+_script='source <(curl -s  http://giteaz:3000/bal/bash-simplify/raw/commit/5bc5e5ee4dffae077f76b1baa177273ce89843a8/dir_util.sh) ; getCurScriptDirByConcat _dir _fn && echo "D=${CurScriptDir}; F=${CurScriptNm}" '
 _script="${_script/_dir/$(pwd)}"
 _script="${_script/_fn/$0}"
 eval "$(bash -c "$_script")"
 echo ${D}, ${F}
 
 #####
+cd ${D}
 
 
 VENV_HOME=${D}/.venv
