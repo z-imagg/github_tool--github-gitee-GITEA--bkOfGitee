9f2fc3c3bf7e39d350f939028ab98bb9b3e48ca8
prgrmz07 <prgrmz07@163.com>
2024-03-15 12:13:42 +0800

    【tool草稿中】添加vscode的py调试配置launch.json

--- tool/submodule_list.py
+++ tool/submodule_list.py
@@ -10,10 +10,11 @@ import shutil
 import sys
 # print(sys.argv)
 CurScriptNm:str=sys.argv[0]
+assert sys.argv[1:].__len__() == 1, "断言错误，参数个数必须等于1. 【用法为】python3 me.py git仓库路径"
+_BgRp:str=sys.argv[1]
 
 
 
-_BgRp:str="/fridaAnlzAp/pytorch"
 BgRp:git.Repo=git.Repo(path=_BgRp)
 BgRpRmt:git.Remote=BgRp.remote()
 originUrlBgRp:str=BgRpRmt.url
