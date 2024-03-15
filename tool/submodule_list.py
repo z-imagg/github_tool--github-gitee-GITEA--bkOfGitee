8a8fb7bdee94ddf430bfa54920ea5b85d1a7f827
prgrmz07 <prgrmz07@163.com>
2024-03-15 12:54:44 +0800

    【草稿 】遍历给定git仓库中的子模块列表， 并打印对应命令import_githubRepo_to_gitee.sh

--- tool/submodule_list.py
+++ tool/submodule_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: <encoding name> -*-
 
+# 【文件作用】 遍历给定git仓库中的子模块列表， 并打印对应命令import_githubRepo_to_gitee.sh
 
 from pathlib import Path
 import git
@@ -11,9 +12,9 @@ import re
 import sys
 # print(sys.argv)
 CurScriptNm:str=sys.argv[0]
-assert sys.argv[1:].__len__() == 1, "断言错误，参数个数必须等于1. 【用法为】python3 me.py git仓库路径"
+assert sys.argv[1:].__len__() == 2, "断言错误，参数个数必须等于2. 【用法为】python3 me.py 父git仓库路径 导入命令休眠间隔秒数"
 _BgRp:str=sys.argv[1]
-
+_sleepSeconds:str=int(sys.argv[2])
 
 
 BgRp:git.Repo=git.Repo(path=_BgRp)
@@ -26,7 +27,7 @@ giteeMirrorOrgName="imagg"
 #                    //host/org /repo
 urlReExpr=r"http[s]?://(.+)/(.+)/(.+)\.git"
 for k,repoK in enumerate( BgRp.submodules):
-    #import_githubRepo_to_gitee.sh --from_repo https://github.com/pytorch/pytorch.git  --goal_org imagg  --goal_repoPath pytorch--pytorch --goal_repoName pytorch--pytorch  --goal_repoDesc 来源https://github.com/pytorch/pytorch.git
+    # print(f"{repoK.name}, {repoK.path}, {repoK.url}, {repoK.hexsha}, {repoK.branch_name}, {repoK.branch_path}")
     urlMatch=re.match(pattern=urlReExpr,string=repoK.url)
     assert urlMatch is not None,f"断言失败，【{repoK.url}】不匹配正则表达式【${urlReExpr}】"
     urlFieldLs=urlMatch.groups()
@@ -35,7 +36,8 @@ for k,repoK in enumerate( BgRp.submodules):
     orgName=urlFieldLs[1]
     repoName=urlFieldLs[2]
     newRepoName=f"{orgName}--{repoName}"
-    cmd_import2gitee=f"import_githubRepo_to_gitee.sh --from_repo {repoK.url}  --goal_org ${giteeMirrorOrgName}  --goal_repoPath ${newRepoName} --goal_repoName ${newRepoName}  --goal_repoDesc 【镜像】{repoK.url}"
-    print(f"{repoK.name}, {repoK.path}, {repoK.url}, {repoK.hexsha}, {repoK.branch_name}, {repoK.branch_path}")
+    #import_githubRepo_to_gitee.sh --from_repo https://github.com/pytorch/pytorch.git  --goal_org imagg  --goal_repoPath pytorch--pytorch --goal_repoName pytorch--pytorch  --goal_repoDesc 来源https://github.com/pytorch/pytorch.git
+    cmd_import2gitee=f"import_githubRepo_to_gitee.sh --from_repo {repoK.url}  --goal_org ${giteeMirrorOrgName}  --goal_repoPath ${newRepoName} --goal_repoName ${newRepoName}  --goal_repoDesc 【镜像】{repoK.url}; sleep ${_sleepSeconds}"
+    print(cmd_import2gitee)
 
 _end=True
