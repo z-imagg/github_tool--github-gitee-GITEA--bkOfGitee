2f5aba514b2df44baa221b4905b40507233e699e
prgrmz07 <prgrmz07@163.com>
2024-03-17 01:18:06 +0800

    克隆进度条tqdm

--- py_util/GitPyCloneProgress.py
+++ py_util/GitPyCloneProgress.py
@@ -1,23 +1,17 @@
 import git
 
-from alive_progress import alive_bar
+from tqdm import tqdm
 
 class GitPyCloneProgressC(git.remote.RemoteProgress):
     def __init__(self):
         super().__init__()
-        self.limit=100
-        self.bar=alive_bar(self.limit, manual=True)
-        self.bar.__enter__()
+        self.pbar = tqdm()
 
 
     def update(self, op_code, cur_count, max_count=None, message=''):
-        if len(message) != 0:
-            # print(op_code, cur_count, max_count, message)
-            pass
-        rate:float=cur_count/max_count
-        self.bar(rate*self.limit)
-        # if cur_count==max_count:
-        #     self.bar.__exit__(None,None,None)
-        # print(self._cur_line)
+        self.pbar.total = max_count
+        self.pbar.n = cur_count
+        self.pbar.refresh()
+
           
 # repo = git.Repo.clone_from(url="git@gitee/xx/xx.git", to_path="xx/xx/xx", progress=GitPyCloneProgressC())
