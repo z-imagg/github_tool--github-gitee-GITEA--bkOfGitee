9c6e395e213a5d17c43d77ba2dd42265e166d8f3
prgrmz07 <prgrmz07@163.com>
2024-03-17 01:53:57 +0800

    导入进度有了

--- py_util/DirUtil.py
+++ py_util/DirUtil.py
@@ -1,6 +1,10 @@
 from pathlib import Path
+import shutil
 
 def dirIsEmptyExcludeHidden(dir:str)->bool:
     ls=list(Path(dir).glob("[!.]*"))
     has:bool = (ls is not None and len(ls)>0)
     return not has
+
+def rmDirRecurse(dir:str):
+    shutil.rmtree(path=dir,ignore_errors=True)
--- py_util/LoopCloneWait.py
+++ py_util/LoopCloneWait.py
@@ -2,17 +2,20 @@ import git
 from GitPyCloneProgress import GitPyCloneProgressC
 import time
 from IdUtil import basicUqIdF
-from DirUtil import dirIsEmptyExcludeHidden
+from DirUtil import dirIsEmptyExcludeHidden,rmDirRecurse
 def loop_clone_wait_F(repoUrl:str)->git.Repo:
-    dir=f"/tmp/{basicUqIdF()}"
     while True:
         try:
+            dir=f"/tmp/loop_clone_wait_F_{basicUqIdF()}"
             repo:git.Repo=git.Repo.clone_from(url=repoUrl,to_path=dir,  progress=GitPyCloneProgressC())
             if dirIsEmptyExcludeHidden(dir): #克隆到的是空仓库。 gitee 导入仓库逻辑， 收到请求后 立即创建一个空仓库，然后跑任务慢慢塞
-                time.sleep(2)
+                time.sleep(5)
+                rmDirRecurse(dir)
                 print(".",end="")#等待中
                 continue
-            return repo
+            else:
+                return repo
         except git.GitCommandError as e:#克隆仓库报错
-            time.sleep(2)
-        print("*",end="")
+            time.sleep(5)
+        rmDirRecurse(dir)
+        print("x",end="")
