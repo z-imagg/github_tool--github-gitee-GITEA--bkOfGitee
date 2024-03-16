a6f4afe756fd2aaa3d524f1b42e49e825adf9ebf
prgrmz07 <prgrmz07@163.com>
2024-03-17 00:26:42 +0800

    草稿中

--- git_submodule_import_cmd_gen/RepoRecurseImport.py
+++ git_submodule_import_cmd_gen/RepoRecurseImport.py
@@ -28,19 +28,19 @@ def main_cmd():
 
     parser.add_argument('-f', '--from_repo_url',required=True,type=str,help="【父仓库url,常为gitee仓库】",metavar='')
     parser.add_argument('-o', '--goal_org',required=True,type=str,help="【目标，gitee的组织】",metavar='')
-    # parser.add_argument('-s', '--sleep_seconds',required=True,type=int,help=f"【 相邻两个子模块导入命令间休眠秒数】最小【{MINI_sleep_seconds}秒】",metavar='')
+    parser.add_argument('-s', '--sleep_seconds',required=True,type=int,help=f"【 相邻两个子模块导入命令间休眠秒数】 ",metavar='')
     args=parser.parse_args()
 
 
-    importGithubRepo2GiteeRecurse(from_repo_url=args.from_repo_url,giteeMirrOrg=args.goal_org)
+    importGithubRepo2GiteeRecurse(from_repo_url=args.from_repo_url,giteeMirrOrg=args.goal_org,sleep_seconds=args.sleep_seconds)
 
-def importGithubRepo2GiteeRecurse(from_repo_url:str,giteeMirrOrg:str):
+def importGithubRepo2GiteeRecurse(from_repo_url:str,giteeMirrOrg:str,sleep_seconds:int=2):
     assert from_repo_url.startswith("https://github.com"), "断言失败，只允许github.com的仓库导入到gitee"
     repoUrlO:GitRepoUrlC=gitRepoUrlParseF(repoUrl=from_repo_url)
 
     newRepoName=f"{repoUrlO.orgName}--{repoUrlO.repoName}"
     simpleRespI:SimpleRespI=gitee_import_repo_wrap_F(fromRepoUrl=from_repo_url,mirrOrg=giteeMirrOrg,newRepoName=newRepoName)
-    time.sleep(2); print("调用gitee的接口后休眠2秒")
+    time.sleep(sleep_seconds); print("调用gitee的接口后休眠2秒")
     mirrRepoUrl:str=simpleRespI.goal_repoUrl
 
     #以 循环克隆仓库 等待 gitee导入仓库任务 完毕
