60b1a2f6d7f963552de21bffbdc27bb90352c739
prgrmz07 <prgrmz07@163.com>
2024-03-17 02:06:12 +0800

    第一次更新进度时，才初始化进度条。 不然，空仓库也会初始化进度条

--- py_util/GitPyCloneProgress.py
+++ py_util/GitPyCloneProgress.py
@@ -5,10 +5,12 @@ from tqdm import tqdm
 class GitPyCloneProgressC(git.remote.RemoteProgress):
     def __init__(self):
         super().__init__()
-        self.pbar = tqdm()
-
 
     def update(self, op_code, cur_count, max_count=None, message=''):
+        if getattr(self,"pbar",None) is None:
+            #第一次更新进度时，才初始化进度条。 不然，空仓库也会初始化进度条
+            self.pbar = tqdm()
+
         self.pbar.total = max_count
         self.pbar.n = cur_count
         self.pbar.refresh()
