6c3e75d844d04a880f25798352886eaacb194b45
prgrmz07 <prgrmz07@163.com>
2024-03-17 01:15:08 +0800

    用不惯alive_progress

--- py_util/GitPyCloneProgress.py
+++ py_util/GitPyCloneProgress.py
@@ -7,6 +7,8 @@ class GitPyCloneProgressC(git.remote.RemoteProgress):
         super().__init__()
         self.limit=100
         self.bar=alive_bar(self.limit, manual=True)
+        self.bar.__enter__()
+
 
     def update(self, op_code, cur_count, max_count=None, message=''):
         if len(message) != 0:
@@ -14,6 +16,8 @@ class GitPyCloneProgressC(git.remote.RemoteProgress):
             pass
         rate:float=cur_count/max_count
         self.bar(rate*self.limit)
+        # if cur_count==max_count:
+        #     self.bar.__exit__(None,None,None)
         # print(self._cur_line)
           
 # repo = git.Repo.clone_from(url="git@gitee/xx/xx.git", to_path="xx/xx/xx", progress=GitPyCloneProgressC())
