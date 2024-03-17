c1c1aeb4dd152feb4d435339d10a695c8dbcb376
prgrmz07 <prgrmz07@163.com>
2024-03-17 20:51:58 +0800

    文案

--- import2gitee/RepoRecurseImport.py
+++ import2gitee/RepoRecurseImport.py
@@ -38,7 +38,7 @@ def printFrmRepoMsg(from_repo_url:str, from_commit_id:str, repo:git.Repo,cntr:Co
     subRpLsTxt=f"有子仓库{len(repo.submodules)}个=【{subNmLs}】" if len(repo.submodules)>0 else "无子仓库"
     tagNmLsTxt,tagLnLs=tagNameLsByCmtId(repo,from_commit_id)
     tagTxt:str=f"tag们【{tagNmLsTxt}】" if len(tagLnLs)>0 else "无tag"
-    print(f"第{cntr.inc()}个仓库 ，【{from_repo_url} {subRpLsTxt}】 ，提交【{from_commit_id}】，{tagTxt}，消息【{repo.commit(from_commit_id).message}】")
+    print(f"第{cntr.inc()}个仓库 【{from_repo_url}】， {subRpLsTxt} ，提交【{from_commit_id}】，{tagTxt}，消息【{repo.commit(from_commit_id).message}】")
     
 
 def main_cmd():
