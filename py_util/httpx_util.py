bf424c00af1bd0d352a1a0556f5eedef033d11d6
prgrmz07 <prgrmz07@163.com>
2024-03-17 12:37:33 +0800

    迁移草稿中

--- py_util/httpx_util.py
+++ py_util/httpx_util.py
@@ -5,6 +5,12 @@ def httpx_post_json(apiUrl:str,reqBodyDct:typing.Dict,timeoutSecs:int=5)->httpx.
   try:
     resp:httpx.Response=httpx.post(url=apiUrl,json=reqBodyDct,verify=False,timeout=timeoutSecs)
     return resp
-  except ConnectionRefusedError as cre:
+  except (httpx.ConnectError) as cre:
     print(f"【疑似服务没开启】{apiUrl}")
     raise cre
+  except (Exception) as exp:
+    #开发调试用
+    print(f"【其他异常】{apiUrl}")
+    import traceback
+    traceback.print_exception(exp)
+    raise exp
