e3be33cf792172298e8e4543651f2770362970ba
prgrmz07 <prgrmz07@163.com>
2024-03-17 20:36:07 +0800

    gitpython获取给定commitId上的tag们（对所有tag进行过滤）

--- py_util/GitPyUtil.py
+++ py_util/GitPyUtil.py
@@ -8,6 +8,6 @@ import typing
 #gitpython获取给定commitId上的tag们（对所有tag进行过滤）
 def tagNameLsByCmtId(repo:git.Repo,cmtId:str )->typing.Tuple[str,typing.List[git.Tag]]:
     tagLs:typing.List[git.Tag]=list(filter(lambda tag:tag.commit.hexsha == cmtId,repo.tags))
-    tagNmLs:typing.List[str]=[tagK.tag.tag  for tagK in tagLs]
-    tagNmLsTxt:str= ",".join(tagLs)
+    tagNmLs:typing.List[str]=[tagK.name  for tagK in tagLs]
+    tagNmLsTxt:str= ",".join(tagNmLs)
     return (tagNmLsTxt,tagLs)
