278e4353de2ee619060c1d8aae5cf7587a9ea2cd
prgrmz07 <prgrmz07@163.com>
2024-03-18 14:32:44 +0800

    rich 克隆进度条正常更新

--- py_util/GitPyCloneProgress.py
+++ py_util/GitPyCloneProgress.py
@@ -10,11 +10,19 @@ class GitPyCloneProgressC(git.remote.RemoteProgress):
         super().__init__()
         #进度条标题
         self.prgrsNm:str=f"{prgrsNm},GPCPC,"
-        self.task_id=getGlbVarInst().richPrgrs.add_task(description=self.prgrsNm)
+        self.task_id=None
+        self.cur_opCode:int=None
+        print(">")
 
     def update(self, op_code, cur_count, max_count=None, message=''):
         # print(".",end="")
         richPrgrs=getGlbVarInst().richPrgrs
+        if self.cur_opCode is None or self.cur_opCode != op_code:
+            self.cur_opCode = op_code
+            # if self.task_id is not None:  richPrgrs.update(task_id=self.task_id,visible=False)
+
+            self.task_id=richPrgrs.add_task(description=f"{self.prgrsNm},{self.cur_opCode}",total=max_count)
+
         # thrdId=threading.get_ident(); print(f"thrdId@prgs={thrdId}")
         #gitpython独自拥有多个线程，因为走到这里多了多个线程
         richPrgrs.update(task_id=self.task_id, advance=cur_count,total=max_count)
