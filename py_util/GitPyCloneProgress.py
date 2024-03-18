7b0f177dc857e560a5ecbca75d602e18bfdb93d1
prgrmz07 <prgrmz07@163.com>
2024-03-18 14:05:47 +0800

    删除rich 4

--- py_util/GitPyCloneProgress.py
+++ py_util/GitPyCloneProgress.py
@@ -8,15 +8,11 @@ class GitPyCloneProgressC(git.remote.RemoteProgress):
         super().__init__()
         #进度条标题
         self.prgrsNm:str=f"{prgrsNm},GPCPC,"
-        self.task_id=getGlbVarInst().richPrgrs.add_task(description=self.prgrsNm)
 
     def update(self, op_code, cur_count, max_count=None, message=''):
-        # print(".",end="")
-        richPrgrs=getGlbVarInst().richPrgrs
+        print("#",end="")
         # thrdId=threading.get_ident(); print(f"thrdId@prgs={thrdId}")
         #gitpython独自拥有多个线程，因为走到这里多了多个线程
-        richPrgrs.update(task_id=self.task_id, advance=cur_count,total=max_count)
 
 
-          
 # repo = git.Repo.clone_from(url="git@gitee/xx/xx.git", to_path="xx/xx/xx", progress=GitPyCloneProgressC())
