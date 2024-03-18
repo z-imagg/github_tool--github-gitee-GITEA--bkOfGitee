e8de079ccdd2558e59a6a85da51621df973da706
prgrmz07 <prgrmz07@163.com>
2024-03-18 19:12:53 +0800

    进度条名字整理， 进度条 创建后 不能直接修改标题，否则 完全乱套了

--- py_util/GitPyCloneProgress.py
+++ py_util/GitPyCloneProgress.py
@@ -17,14 +17,12 @@ class GitPyCloneProgressC(RemoteProgress):
         self.cur_gitOpCode:int=None
         self.gitOpCodeLs:typing.List[int]=[]
         print(">")
-        self.bar:Bar=None
+        self.prgrsBar:Bar=None
         self.opCodeLsTxt:str=None
 
     def update(self, gitOpCode:int, cur_count:typing.Union[str, float], max_count:typing.Union[str, float, None]=None, message:str=''):
         print("#",end="")
         
-        # bar = Bar()
-        # bar.goto()
         #保存有史以来的所有git操作码， gitOpCode按照来的时刻保持顺序
         if gitOpCode not in self.gitOpCodeLs:
             self.gitOpCodeLs.append(gitOpCode)
@@ -36,17 +34,18 @@ class GitPyCloneProgressC(RemoteProgress):
 
             # 新建进度条
             #    参考 https://github.com/verigak/progress/
-            self.bar:Bar=Bar(f"{self.prgrsNm},gitOpCd【{gitOpCode}】", max=max_count,fill="!", suffix="【%(percent)d％;%(index)d;%(max)d】")
+            self.prgrsBar:Bar=Bar(f"{self.prgrsNm},gitOpCd【{gitOpCode}】", max=max_count,fill="!", suffix="【%(percent)d％;%(index)d;%(max)d】")
 
         self.opCodeLsTxt:str=">".join([f"{k}" for k in self.gitOpCodeLs])
 
+        # 进度条 创建后 不能直接修改标题，否则 完全乱套了
         # self.countDown.message=f"{self.countDown.message},{opCodeLsTxt}"
           
         #更新当前进度条
-        self.bar.goto(cur_count)
+        self.prgrsBar.goto(cur_count)
 
         #数字到达的时候，进度条调用finish方法 以 结束，否则不换行
-        if cur_count == max_count: self.bar.finish()
+        if cur_count == max_count: self.prgrsBar.finish()
 
 
 # repo = git.Repo.clone_from(url="git@gitee/xx/xx.git", to_path="xx/xx/xx", progress=GitPyCloneProgressC())
