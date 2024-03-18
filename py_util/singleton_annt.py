b45d0169ca60b15eb9dfef29f57bf0b9bfbd8356
prgrmz07 <prgrmz07@163.com>
2024-03-18 10:51:57 +0800

    【正常】【加注释】 Python 装饰器线程安全单例的实现(使用函数装饰器)

--- py_util/singleton_annt.py
+++ py_util/singleton_annt.py
@@ -15,7 +15,7 @@ def SngltAnnt(cls):
     lock = threading.Lock()
 
     def __single_with_thread_lock(*args,**kwargs):
-        print("lock" + str(lock))
+        # print("lock" + str(lock)) #全都是同一个lock
         if cls not in instance:
             with lock:
                 if cls not in instance:
@@ -25,16 +25,23 @@ def SngltAnnt(cls):
 
 @SngltAnnt
 class __C000__():
-    def __init__(self):
+    def __init__(self,age:int):
+        #此__init__方法只会进入一次
+        self.m_age:int=age
         time.sleep(2)
+        print(f"self.m_age={self.m_age}")
 
 def printNewInstance():
-    print(__C000__())
+    import random
+    inst=__C000__(random.randint(4,10))
+    # print(inst) #都是类__C000__的同一个实例
+    print(f"inst.m_age={inst.m_age}") #这里打印的都是99
 
 
 
 #以下是测试单例模式
 if __name__=="__main__":
+    __C000__(99)#此行为第一次调用  方法__C000__.__init__ , 会实际进入到 方法__C000__.__init__
     for i in range(20):
         threading.Thread(target=printNewInstance).start()
     
