216a383913e2318efcb37653fae0a30429f1d28a
prgrmz07 <prgrmz07@163.com>
2024-03-18 14:42:51 +0800

    GitPyCloneProgressC.update方法签名完善

--- py_util/GitPyCloneProgress.py
+++ py_util/GitPyCloneProgress.py
@@ -1,11 +1,13 @@
 import git
+from git.remote import RemoteProgress
 from rich import print
 from rich.progress import Progress,TaskID
 import threading
+import typing
 
 from global_var import getGlbVarInst
 
-class GitPyCloneProgressC(git.remote.RemoteProgress):
+class GitPyCloneProgressC(RemoteProgress):
     def __init__(self,prgrsNm:str):
         super().__init__()
         #进度条标题
@@ -14,7 +16,7 @@ class GitPyCloneProgressC(git.remote.RemoteProgress):
         self.cur_opCode:int=None
         print(">")
 
-    def update(self, op_code, cur_count, max_count=None, message=''):
+    def update(self, op_code:int, cur_count:typing.Union[str, float], max_count:typing.Union[str, float, None]=None, message:str=''):
         # print(".",end="")
         richPrgrs=getGlbVarInst().richPrgrs
         if self.cur_opCode is None or self.cur_opCode != op_code:
