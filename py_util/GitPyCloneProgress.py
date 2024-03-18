aac6124a938ccc71418b5b4b8e2ad3e4df0cec5d
prgrmz07 <prgrmz07@163.com>
2024-03-18 18:35:19 +0800

    草稿进度条

--- py_util/GitPyCloneProgress.py
+++ py_util/GitPyCloneProgress.py
@@ -17,19 +17,10 @@ class GitPyCloneProgressC(RemoteProgress):
         print(">")
         self.countDown:Countdown=None
         self.counter:Counter=None
+        self.opCodeLsTxt:str=None
 
     def update(self, gitOpCode:int, cur_count:typing.Union[str, float], max_count:typing.Union[str, float, None]=None, message:str=''):
         print("#",end="")
-        
-        if  self.countDown is None:
-            self.countDown:Countdown=Countdown("克隆进度倒数", max=max_count)
-
-        # if  self.counter is None:
-        #     self.counter:Counter=Counter(f"克隆进度正数{max_count}", max=max_count)
-        
-        # self.counter.next
-        self.countDown.goto(cur_count)
-        
         
         #保存有史以来的所有git操作码， gitOpCode按照来的时刻保持顺序
         if gitOpCode not in self.gitOpCodeLs:
@@ -40,17 +31,18 @@ class GitPyCloneProgressC(RemoteProgress):
             # 更新 当前git操作码
             self.cur_gitOpCode = gitOpCode
             # 隐藏上一个进度条
-            # if self.task_id is not None:  pass#TODO
+            if self.countDown is not None:  
+                self.countDown.finish()
 
             # 新建进度条
-            # self.task_id=TODO
+            self.countDown:Countdown=Countdown(f"克隆进度倒数【{gitOpCode}】【{max_count}】", max=max_count)
 
-        opCodeLsTxt:str=">".join([f"{k}" for k in self.gitOpCodeLs])
+        self.opCodeLsTxt:str=">".join([f"{k}" for k in self.gitOpCodeLs])
 
         # self.countDown.message=f"{self.countDown.message},{opCodeLsTxt}"
           
         #更新当前进度条
-        #TODO cur_count,max_count
+        self.countDown.goto(cur_count)
 
 
 # repo = git.Repo.clone_from(url="git@gitee/xx/xx.git", to_path="xx/xx/xx", progress=GitPyCloneProgressC())
--- py_util/LoopCloneWait.py
+++ py_util/LoopCloneWait.py
@@ -13,7 +13,8 @@ def loop_clone_wait_F(repoUrl:str,title:str)->git.Repo:
         try:
             progressTitle=f"【{title},{repoUrl},{idx}_clone】"
             dir=f"/tmp/loop_clone_wait_F_{basicUqIdF()}"
-            repo:git.Repo=git.Repo.clone_from(url=repoUrl,to_path=dir,  progress=GitPyCloneProgressC(progressTitle ))
+            progressObj=GitPyCloneProgressC(progressTitle )
+            repo:git.Repo=git.Repo.clone_from(url=repoUrl,to_path=dir,  progress=progressObj)
             if dirIsEmptyExcludeHidden(dir): #克隆到的是空仓库。 gitee 导入仓库逻辑， 收到请求后 立即创建一个空仓库，然后跑任务慢慢塞
                 sleepVerbose( randSecs(5) ,title)
                 rmDirRecurse(dir)
@@ -21,6 +22,7 @@ def loop_clone_wait_F(repoUrl:str,title:str)->git.Repo:
                 continue
             else:
                 #正常克隆仓库
+                print(f"progressObj {progressObj.opCodeLsTxt}")
                 return repo
         except git.GitCommandError as e:#克隆仓库报错
             sleepVerbose( randSecs(5) ,f"【2】{title}")
