b4237fcf5b243513ab5e98140940dea14e9a7dc8
prgrmz07 <prgrmz07@163.com>
2024-03-18 14:50:37 +0800

    进度条 注释

--- py_util/GitPyCloneProgress.py
+++ py_util/GitPyCloneProgress.py
@@ -19,17 +19,26 @@ class GitPyCloneProgressC(RemoteProgress):
 
     def update(self, gitOpCode:int, cur_count:typing.Union[str, float], max_count:typing.Union[str, float, None]=None, message:str=''):
         # print(".",end="")
+        richPrgrs=getGlbVarInst().richPrgrs
+        
+        #保存有史以来的所有git操作码
         self.gitOpCodeLs.add(gitOpCode)
         opCodeLsTxt:str="、".join([f"{k}" for k in self.gitOpCodeLs])
-        richPrgrs=getGlbVarInst().richPrgrs
+        
+        #若 来了第一个git操作码 或 git操作码 变化了
         if self.cur_gitOpCode is None or self.cur_gitOpCode != gitOpCode:
+            # 更新 当前git操作码
             self.cur_gitOpCode = gitOpCode
+            # 隐藏上一个进度条
             if self.task_id is not None:  richPrgrs.update(task_id=self.task_id,visible=False)
 
+            # 新建进度条
             self.task_id=richPrgrs.add_task(description=f"{self.prgrsNm},{opCodeLsTxt}",total=max_count)
 
         # thrdId=threading.get_ident(); print(f"thrdId@prgs={thrdId}")
         #gitpython独自拥有多个线程，因为走到这里多了多个线程
+        
+        #更新当前进度条
         richPrgrs.update(task_id=self.task_id, advance=cur_count,total=max_count)
 
 
