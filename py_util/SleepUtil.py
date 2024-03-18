bce77cf2de1b8095896face125b784e50d4b4f1e
prgrmz07 <prgrmz07@163.com>
2024-03-18 14:04:56 +0800

    删除rich 3

--- py_util/SleepUtil.py
+++ py_util/SleepUtil.py
@@ -8,6 +8,8 @@ def sleepVerbose(seconds:int, title:str ):
     oneUnit_ms:float=0.5 #500毫秒
     unitCnt=int(seconds//oneUnit_ms)
     
-    for k in getGlbVarInst().richPrgrs.track(sequence= range(0, unitCnt),description=f"{title}【{seconds}秒】" ):
-        # print(z,end="")
+    description=f"{title}【{seconds}秒】"
+    print(description)
+    for k in range(0, unitCnt):
+        print(".",end="")
         time.sleep(oneUnit_ms)
