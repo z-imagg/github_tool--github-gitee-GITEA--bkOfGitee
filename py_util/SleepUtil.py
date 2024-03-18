e7d0bd0d3380895463f8ba997cd40ce1732e3547
prgrmz07 <prgrmz07@163.com>
2024-03-18 19:02:19 +0800

    草稿 Countdown例子

--- py_util/SleepUtil.py
+++ py_util/SleepUtil.py
@@ -1,14 +1,17 @@
 import time
 
-
+from progress.counter import Countdown
 
 from global_var import GlbVar,getGlbVarInst
 #休眠500毫秒，打印一个字符
 def sleepVerbose(seconds:int, title:str ):
-    oneUnit_ms:float=0.5 #500毫秒
+    oneUnit_ms:float=0.2 #200毫秒
     unitCnt=int(seconds//oneUnit_ms)
+
     
-    description=f"{title}【{seconds}秒】"
+    description=f"{title}【{seconds}秒】,slpVbs,"
+    countDown:Countdown = Countdown(description,max=unitCnt)
     for k in range(0, unitCnt):
-        print(".",end="")
+        countDown.next(1)#自减1
+        # print(".",end="")
         time.sleep(oneUnit_ms)
