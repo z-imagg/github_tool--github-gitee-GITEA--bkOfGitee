89bc82cc12043483cba23346ff83db777299149e
prgrmz07 <prgrmz07@163.com>
2024-03-18 18:26:29 +0800

    删除rich 6

--- py_util/global_var.py
+++ py_util/global_var.py
@@ -12,7 +12,7 @@ class GlbVar:
 #使用函数装饰器 的弊端是  无法获取到 真实类对象 ，从而 无法调用static方法。 只能绕开
 def getGlbVarInst()->GlbVar:
     inst = GlbVar(None)#这句话并不是构造对象，而是获取单例对象
-    assert inst.richPrgrs is not None, "断言失败，必须先手工实例化GlbVar(合理的参数) ，再调用本方法getGlbVarInst获取全局变量"
+    assert inst.global_var1 is not None, "断言失败，必须先手工实例化GlbVar(合理的参数) ，再调用本方法getGlbVarInst获取全局变量"
     return inst
 
 
