746ab28fc33c1f5ab4fc5de78e91d43236d4202a
prgrmz07 <prgrmz07@163.com>
2024-03-18 12:23:01 +0800

    xx

--- py_util/LoopCloneWait.py
+++ py_util/LoopCloneWait.py
@@ -13,7 +13,7 @@ from global_var import GlbVar,getGlbVarInst
 def loop_clone_wait_F(repoUrl:str,title:str)->git.Repo:
     while True:
         try:
-            progressTitle=f"【loop_clone_wait_F,{repoUrl}】"
+            progressTitle=f"【{title},{repoUrl}】"
             dir=f"/tmp/loop_clone_wait_F_{basicUqIdF()}"
             repo:git.Repo=git.Repo.clone_from(url=repoUrl,to_path=dir,  progress=GitPyCloneProgressC(progressTitle,getGlbVarInst().richPrgrs))
             if dirIsEmptyExcludeHidden(dir): #克隆到的是空仓库。 gitee 导入仓库逻辑， 收到请求后 立即创建一个空仓库，然后跑任务慢慢塞
