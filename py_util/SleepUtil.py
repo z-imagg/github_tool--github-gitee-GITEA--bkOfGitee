6ae96f68320f4cb6da8894b184c5e89da041cc80
prgrmz07 <prgrmz07@163.com>
2024-03-18 19:08:45 +0800

    草稿 Countdown例子， 调用finish才会换行正常，否则不换行

--- py_util/SleepUtil.py
+++ py_util/SleepUtil.py
@@ -5,13 +5,14 @@ from progress.counter import Countdown
 from global_var import GlbVar,getGlbVarInst
 #休眠500毫秒，打印一个字符
 def sleepVerbose(seconds:int, title:str ):
-    oneUnit_ms:float=0.2 #200毫秒
-    unitCnt=int(seconds//oneUnit_ms)
-
+    total_ms=seconds*1000
+    unit_ms:float=200 #200毫秒
     
     description=f"{title}【{seconds}秒】,slpVbs,"
-    countDown:Countdown = Countdown(description,max=unitCnt)
-    for k in range(0, unitCnt):
-        countDown.next(1)#自减1
+    countDown:Countdown = Countdown(description,max=total_ms) #Countdown没有参数  suffix='%(percent)d%%'，  Bar才有该参数
+    for k in range(0, total_ms, unit_ms):
+        countDown.next(unit_ms)#自减1个单位
         # print(".",end="")
-        time.sleep(oneUnit_ms)
+        time.sleep(unit_ms/1000)
+    
+    countDown.finish()
