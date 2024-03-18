eac8a617d6d9f8f636a66e66de8ff9cf5a167ff4
prgrmz07 <prgrmz07@163.com>
2024-03-18 10:02:41 +0800

    修复休眠的逻辑错误

--- py_util/SleepUtil.py
+++ py_util/SleepUtil.py
@@ -7,4 +7,4 @@ def sleepVerbose(seconds:int, z:str ):
     unit_ms:float=0.5 #500毫秒
     for k in range(0, int(seconds//unit_ms)):
         print(z,end="")
-        time.sleep(seconds)
+        time.sleep(unit_ms)
