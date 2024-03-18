328e7e20d7aa8952f733c12228934320d0af5408
prgrmz07 <prgrmz07@163.com>
2024-03-18 18:03:50 +0800

    删除rich 3

--- py_util/SleepUtil.py
+++ py_util/SleepUtil.py
@@ -8,8 +8,7 @@ def sleepVerbose(seconds:int, title:str ):
     oneUnit_ms:float=0.5 #500毫秒
     unitCnt=int(seconds//oneUnit_ms)
     
-    # 全局只能有一个 rich.progress.Progress的实例，
-    # 所以  这里必须要写 richPrgrs.track, 而直接写track, 否则会报错  " rich.errors.LiveError: Only one live display may be active at once ", 
-    for k in getGlbVarInst().richPrgrs.track(sequence= range(0, unitCnt),description=f"{title}【{seconds}秒】" ):
-        # print(z,end="")
+    description=f"{title}【{seconds}秒】"
+    for k in range(0, unitCnt):
+        print(".",end="")
         time.sleep(oneUnit_ms)
