4cdc804a7c697b0768dadddd49aadb59adeac911
prgrmz07 <prgrmz07@163.com>
2024-03-18 11:53:05 +0800

    构造单例对象后以外部方法充当static方法getInstance

--- py_util/global_var.py
+++ py_util/global_var.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: <encoding name> -*-
 
-
-from singleton_annt import SngltAnnt
+from singleton_annt import funcSngltAnnt
 from rich.progress import Progress
 
-@SngltAnnt
-class GlbVar():
+@funcSngltAnnt
+class GlbVar:
     def __init__(self,richPrgrs:Progress):
         self.richPrgrs:Progress=richPrgrs
 
-    @staticmethod
-    def getInst()->'GlbVar':
-        return GlbVar()
-    
+
+#使用函数装饰器 的弊端是  无法获取到 真实类对象 ，从而 无法调用static方法。 只能绕开
+def getGlbVarInst()->GlbVar:
+    inst = GlbVar(None)#这句话并不是构造对象，而是获取单例对象
+    assert inst.richPrgrs is not None, "断言失败，必须先手工实例化GlbVar(合理的参数) ，再调用本方法getGlbVarInst获取全局变量"
+    return inst
+
 
 #测试
 if __name__=="__main__":
     with Progress() as richPrgrs:
-        GlbVar(richPrgrs=richPrgrs)
+        inst=GlbVar(richPrgrs=richPrgrs)
+        inst2=getGlbVarInst()
         end=True
