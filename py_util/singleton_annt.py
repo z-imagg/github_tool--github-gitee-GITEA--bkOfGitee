c353890c7ecd65a4d3b4de380e97f6bb62de25c0
prgrmz07 <prgrmz07@163.com>
2024-03-18 11:24:51 +0800

    注释

--- py_util/singleton_annt.py
+++ py_util/singleton_annt.py
@@ -14,7 +14,7 @@ def SngltAnnt(cls):
     instance = {}
     lock = threading.Lock()
 
-    def __single_with_thread_lock(*args,**kwargs): # args、kwargs 中放的是 __C000__.__init__的参数们
+    def __single_with_thread_lock(*args,**kwargs): # args、kwargs 中放的是 __BuszClz000__.__init__的参数们
         # print("lock" + str(lock)) #全都是同一个lock
         if cls not in instance:
             with lock:
@@ -24,7 +24,7 @@ def SngltAnnt(cls):
     return __single_with_thread_lock
 
 @SngltAnnt
-class __C000__():
+class __BuszClz000__():
     def __init__(self,age:int):
         #此__init__方法只会进入一次
         self.m_age:int=age
@@ -33,7 +33,7 @@ class __C000__():
 
 def printNewInstance():
     import random
-    inst=__C000__(random.randint(4,10))
+    inst=__BuszClz000__(random.randint(4,10))
     # print(inst) #都是类__C000__的同一个实例
     print(f"inst.m_age={inst.m_age}") #这里打印的都是99
 
@@ -41,7 +41,7 @@ def printNewInstance():
 
 #以下是测试单例模式
 if __name__=="__main__":
-    __C000__(99)#此行为第一次调用  方法__C000__.__init__ , 会实际进入到 方法__C000__.__init__
+    __BuszClz000__(99)#此行为第一次调用  方法__C000__.__init__ , 会实际进入到 方法__C000__.__init__
     for i in range(20):
         threading.Thread(target=printNewInstance).start()
     
