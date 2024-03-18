e251de65dcb012c18d35b4afac20245b0549e18c
prgrmz07 <prgrmz07@163.com>
2024-03-18 09:49:02 +0800

    【正常显示】rich进度条实验，不用with

--- migrate2GITEA/RepoRecurseMigrate.py
+++ migrate2GITEA/RepoRecurseMigrate.py
@@ -32,20 +32,19 @@ from rich.progress import track
 import time
 
 richPrgrs:Progress = Progress()
+richPrgrs.__enter__()
 cntr:Counter=Counter()
 
 def main_cmd():
-    with Progress() as progress:
+    task1 = richPrgrs.add_task("[red]Downloading...", total=200)
+    task2 = richPrgrs.add_task("[green]Processing...", total=200)
+    task3 = richPrgrs.add_task("[cyan]Cooking...", total=200)
 
-        task1 = progress.add_task("[red]Downloading...", total=200)
-        task2 = progress.add_task("[green]Processing...", total=200)
-        task3 = progress.add_task("[cyan]Cooking...", total=200)
-
-        while not progress.finished:
-            progress.update(task1, advance=0.5)
-            progress.update(task2, advance=0.3)
-            progress.update(task3, advance=0.9)
-            time.sleep(0.02)
+    while not richPrgrs.finished:
+        richPrgrs.update(task1, advance=0.5)
+        richPrgrs.update(task2, advance=0.3)
+        richPrgrs.update(task3, advance=0.9)
+        time.sleep(0.02)
 
     parser = argparse.ArgumentParser(
     prog=f'gitSubmoduleImportCmdGen.py',
@@ -94,3 +93,4 @@ def migrateRecurse(ornRUrl:str, ornCmtId:str, frmBaseUrl:str, frmOrgNm:str, slpS
 
 if __name__=="__main__":
     main_cmd()
+    richPrgrs.__exit__()
