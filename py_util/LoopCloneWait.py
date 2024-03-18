571cbb99f8f9006ad90ec84a8784fcd93b90665f
prgrmz07 <prgrmz07@163.com>
2024-03-18 17:25:59 +0800

    日志调整

--- py_util/LoopCloneWait.py
+++ py_util/LoopCloneWait.py
@@ -17,7 +17,7 @@ def loop_clone_wait_F(repoUrl:str,title:str)->git.Repo:
             dir=f"/tmp/loop_clone_wait_F_{basicUqIdF()}"
             repo:git.Repo=git.Repo.clone_from(url=repoUrl,to_path=dir,  progress=GitPyCloneProgressC(progressTitle ))
             if dirIsEmptyExcludeHidden(dir): #克隆到的是空仓库。 gitee 导入仓库逻辑， 收到请求后 立即创建一个空仓库，然后跑任务慢慢塞
-                sleepVerbose( randSecs(5) ,title)
+                sleepVerbose( randSecs(5) ,f"{title},{idx}")
                 rmDirRecurse(dir)
                 # print(".",end="")#等待中
                 continue
